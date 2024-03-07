23b7f879c19350cd10662b9d0ad46030a2b8659b
Mathew-Estafanous <mathewestafanous13@gmail.com>
2024-03-06 21:10:28 -0500

    chore: improve auth api abstraction

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,9 +1,11 @@
-import {checkJwt} from "./authentication";
 import pino from 'pino';
 import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { UnauthorizedError } from 'express-oauth2-jwt-bearer';
+import { checkJwt } from './authentication';
 
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const logger = pino({
   name: 'tmu-connect-api',
@@ -14,11 +16,7 @@ const logger = pino({
   },
 });
 
-app.listen(port, () => {
-  logger.info('TMU Connect server listening on port 8080!');
-});
-
-app.get('/posts', (req, res) => {
+app.get('/posts', (_req, res) => {
   res.json([
     {
       id: 1,
@@ -34,16 +32,27 @@ app.get('/post/:id', (req, res) => {
   });
 });
 
-app.post('/post', checkJwt, (req, res) => {
-  res.json({
-    id: 2,
-    title: 'Hello World',
-  });
-});
+app.post('/post', checkJwt, asyncHandler(async (_req, res) => {
+  res.send('Hello World');
+}));
 
 app.delete('/post/:id', checkJwt, (req, res) => {
   res.json({
     id: req.params.id,
     title: 'Hello World',
   });
+});
+
+app.use((err: Error, _req: express.Request, res: express.Response) => {
+  logger.error(err.message, err.stack);
+  if (err instanceof UnauthorizedError) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.listen(PORT, () => {
+  logger.info('TMU Connect server listening on PORT 8080.');
 });
--- server/src/authentication.ts
+++ server/src/authentication.ts
@@ -1,6 +1,14 @@
-import {ManagementClient, UserInfoClient} from 'auth0';
+import { ManagementClient, UserInfoClient } from 'auth0';
 import { auth } from 'express-oauth2-jwt-bearer';
-import pino from "pino";
+import pino from 'pino';
+
+export type Auth0User = {
+  auth0Id: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  picture?: string;
+};
 
 const logger = pino({
   name: 'authentication',
@@ -16,8 +24,8 @@ if (!process.env.AUTH0_DOMAIN) {
   logger.error('AUTH0_DOMAIN environment variable not set');
   process.exit();
 }
-if (!process.env.AUHT0_CLIENT_ID) {
-  logger.error('AUHT0_CLIENT_ID environment variable not set');
+if (!process.env.AUTH0_CLIENT_ID) {
+  logger.error('AUTH0_CLIENT_ID environment variable not set');
   process.exit();
 }
 if (!process.env.AUTH0_CLIENT_SECRET) {
@@ -29,10 +37,14 @@ if (!process.env.BACKEND_AUDIENCE) {
   process.exit();
 }
 
-const management = new ManagementClient({
+const userInfoClient = new UserInfoClient({
   domain: process.env.AUTH0_DOMAIN!!,
-  clientId: process.env.AUHT0_CLIENT_ID!!,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET!!,
+});
+
+const manageClient = new ManagementClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
 
 export const checkJwt = auth({
@@ -40,8 +52,34 @@ export const checkJwt = auth({
   issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
 });
 
-const getIdentityUser = async (userId: string) => {
-  management.users.get({ id: userId }).then((user) => user.data);
+const getAuth0User = async (accessToken: string): Promise<Auth0User> => userInfoClient
+  .getUserInfo(accessToken).then((user) => {
+    if (user.status !== 200) {
+      logger.error(user.statusText);
+      throw new Error('Failed to retrieve user info');
+    }
+
+    return {
+      auth0Id: user.data.sub,
+      email: user.data.email,
+      firstName: user.data.given_name,
+      lastName: user.data.family_name,
+      picture: user.data.picture,
+    };
+  });
+
+const updateAuth0User = async (user: Auth0User) => {
+  manageClient.users.update({
+    id: user.auth0Id,
+  }, {
+    ...user,
+  }).then((result) => {
+    if (result.status !== 200) {
+      logger.warn(result.statusText);
+      throw new Error('Failed to update user info');
+    }
+    logger.debug(`Successfully updated user info of ${user.auth0Id}`);
+  });
 };
 
-export default getIdentityUser;
+export default { getAuth0User, updateAuth0User };
