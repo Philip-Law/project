845f92a967f8a63368237baa7db072ab81e4d733
Mathew-Estafanous <mathewestafanous13@gmail.com>
2024-03-08 17:26:48 -0500

    chore: setup logging and change api to index

--- server/src/authentication.ts
+++ server/src/authentication.ts
@@ -1,6 +1,6 @@
 import { ManagementClient, UserInfoClient } from 'auth0';
 import { auth } from 'express-oauth2-jwt-bearer';
-import pino from 'pino';
+import LOGGER from './configs/logging';
 
 export type Auth0User = {
   auth0Id: string;
@@ -10,30 +10,21 @@ export type Auth0User = {
   picture?: string;
 };
 
-const logger = pino({
-  name: 'authentication',
-  level: process.env.PINO_LOG_LEVEL || 'debug',
-  timestamp: pino.stdTimeFunctions.isoTime,
-  redact: {
-    paths: ['email'],
-  },
-});
-
 // Check that all required environment variables are set
 if (!process.env.AUTH0_DOMAIN) {
-  logger.error('AUTH0_DOMAIN environment variable not set');
+  LOGGER.error('AUTH0_DOMAIN environment variable not set');
   process.exit();
 }
 if (!process.env.AUTH0_CLIENT_ID) {
-  logger.error('AUTH0_CLIENT_ID environment variable not set');
+  LOGGER.error('AUTH0_CLIENT_ID environment variable not set');
   process.exit();
 }
 if (!process.env.AUTH0_CLIENT_SECRET) {
-  logger.error('AUTH0_CLIENT_SECRET environment variable not set');
+  LOGGER.error('AUTH0_CLIENT_SECRET environment variable not set');
   process.exit();
 }
 if (!process.env.BACKEND_AUDIENCE) {
-  logger.error('BACKEND_AUDIENCE environment variable not set');
+  LOGGER.error('BACKEND_AUDIENCE environment variable not set');
   process.exit();
 }
 
@@ -55,7 +46,7 @@ export const checkJwt = auth({
 const getAuth0User = async (accessToken: string): Promise<Auth0User> => userInfoClient
   .getUserInfo(accessToken).then((user) => {
     if (user.status !== 200) {
-      logger.error(user.statusText);
+      LOGGER.error(user.statusText);
       throw new Error('Failed to retrieve user info');
     }
 
@@ -75,10 +66,10 @@ const updateAuth0User = async (user: Auth0User) => {
     ...user,
   }).then((result) => {
     if (result.status !== 200) {
-      logger.warn(result.statusText);
+      LOGGER.warn(result.statusText);
       throw new Error('Failed to update user info');
     }
-    logger.debug(`Successfully updated user info of ${user.auth0Id}`);
+    LOGGER.debug(`Successfully updated user info of ${user.auth0Id}`);
   });
 };
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,18 @@
-import pino from 'pino';
 import express from 'express';
 import { UnauthorizedError } from 'express-oauth2-jwt-bearer';
 import AppDataSource from './configs/db';
 import { messageRoutes, postRoutes, userRoutes } from './routes';
+import LOGGER from './configs/logging';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const logger = pino({
-  name: 'tmu-connect-api',
-  level: process.env.PINO_LOG_LEVEL || 'debug',
-  timestamp: pino.stdTimeFunctions.isoTime,
-  redact: {
-    paths: ['email'],
-  },
-});
 
 app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 app.use('/message', messageRoutes);
 
 app.use((err: Error, _req: express.Request, res: express.Response) => {
-  logger.error(err.message, err.stack);
+  LOGGER.error(err.message, err.stack);
   if (err instanceof UnauthorizedError) {
     res.status(401).json({ error: 'Unauthorized' });
     return;
@@ -33,11 +24,11 @@ app.use((err: Error, _req: express.Request, res: express.Response) => {
 AppDataSource.initialize()
   .then(() => {
     // here you can start to work with your database
-    logger.debug('Successfully connected to database');
+    LOGGER.debug('Successfully connected to database');
     app.listen(PORT, () => {
-      logger.info('TMU Connect server listening on PORT 8080.');
+      LOGGER.info('TMU Connect server listening on PORT 8080.');
     });
   })
   .catch((error) => {
-    logger.fatal(`Could not connect to database: ${error}`);
+    LOGGER.fatal(`Could not connect to database: ${error}`);
   });
