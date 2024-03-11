d49569aa8a4211fa9b85ac6d11978b30563cd6df
Jacob Pantuso <jacobpantuso@outlook.com>
2024-03-11 17:31:42 -0400

    Moved location to filter section and made search bar wider.

--- client/src/views/Nav.tsx
+++ client/src/views/Nav.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import '../style/Nav.css'
 import NavUser from '../components/NavUser'
 import Search from '../components/Search'
 
 const Nav = (): React.ReactElement => {
+  const [isOpen, setOpen] = useState(false)
+  const handleNavToggle = (): void => {
+    setOpen(!isOpen)
+  }
   return (
     <div className='nav'>
-        <div className='nav container'>
+        <div id='desktop' className='nav container'>
             <div className='nav child left'>
                 <img src={'/assets/tmu_logo.png'} alt='logo' className='logo' />
                 <h2>TMU Connect</h2>
@@ -18,6 +23,24 @@ const Nav = (): React.ReactElement => {
             <div className='nav child right'>
                 <button className='nav-button'>Post Ad</button>
                 <NavUser/>
+            </div>
+        </div>
+
+        <div id='mobile' className='nav container'>
+            <div className='top-nav'>
+                <div className='mobile-left'>
+                    <img src={'/assets/tmu_logo.png'} alt='logo' className='logo' />
+                    <h2>TMU Connect</h2>
+                </div>
+                <div className='mobile-right'>
+                    <FontAwesomeIcon className={`${isOpen ? 'nav-icon active' : 'nav-icon'}`} icon={faBars} onClick={handleNavToggle}/>
+                </div>
+            </div>
+            <div id={`${isOpen ? 'show' : 'hidden'}`} className='bottom-nav'>
+                <Search />
+                <div className={`nav-user-container ${isOpen ? 'show' : ''}`}>
+                    <NavUser />
+                </div>
             </div>
         </div>
     </div>
