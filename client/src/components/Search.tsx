25af519b77a3fa825704d35b218e1f5e27fabe7c
Jacob Pantuso <jacobpantuso@outlook.com>
2024-03-11 17:31:54 -0400

    Removed dropdown from search bar.

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,61 +1,23 @@
 import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../style/Search.css'
-import { faChevronDown, faChevronUp, faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Search = (): React.ReactElement => {
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-  const [selectedOption, setSelectedOption] = useState('Current Location')
-  const [searchPlaceholder, setSearchPlaceholder] = useState('Search anything...')
+  const [searchValue, setSearchValue] = useState<string>('')
 
-  const handleDropdownToggle = (): void => {
-    setDropdownOpen(!dropdownOpen)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchValue(e.target.value)
   }
 
-  const handleOptionSelect = (option: string): void => {
-    setSelectedOption(option)
-    setSearchPlaceholder(option === 'Everything' ? 'Search Anything...' : `Search in ${option}...`)
-    setDropdownOpen(false)
-  }
-
-  const [location] = useState('current')
   const handleSubmit = (e: React.FormEvent): void => {
     e.preventDefault()
-    console.log(e)
-    if (location === 'current') {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords
-          console.log('Latitude:', latitude)
-          console.log('Longitude:', longitude)
-        },
-        (error) => {
-          console.error('Error getting location:', error.message)
-        }
-      )
-    }
+    console.log(searchValue)
   }
   return (
         <div className='search-container'>
-            <div className='dropdown'>
-                <div className='dropdown-text' onClick={handleDropdownToggle}>
-                        <FontAwesomeIcon icon={faLocationDot}/>
-                        <span>{selectedOption}</span>
-                        <FontAwesomeIcon icon={dropdownOpen ? faChevronDown : faChevronUp } />
-                </div>
-                <ul id='list' className={`dropdown-list ${dropdownOpen ? 'show' : ''}`}>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Current Location') }}>Current Location</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Toronto') }}>Toronto</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Vaughan') }}>Vaughan</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Scarborough') }}>Scarborough</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('North York') }}>North York</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Barrie') }}>Barrie</li>
-                        <li className='dropdown-list-item' onClick={() => { handleOptionSelect('Aurora') }}>Aurora</li>
-
-                </ul>
-            </div>
             <div className='search-box'>
-                <input type='text' id='search-input' placeholder={ searchPlaceholder } />
+                <input type='text' id='search-input' placeholder={ 'What are you looking for?' } onChange={handleChange}/>
                 <FontAwesomeIcon icon={faMagnifyingGlass} onClick={ handleSubmit }/>
             </div>
         </div>
