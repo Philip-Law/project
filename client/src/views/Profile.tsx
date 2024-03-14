bbe293b619697d6850d91bbf55f1a765e20b1e92
JacobPantuso <jacobpantuso@outlook.com>
2024-03-14 19:19:29 -0400

    Implemented databse request.

--- client/src/views/Profile.tsx
+++ client/src/views/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Nav from './Nav'
 import '../style/Profile.css'
 import Listings from '../components/Listings'
@@ -8,35 +8,35 @@ import { faEdit, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Profile = (): React.ReactElement => {
   const { user, isLoading, getAccessTokenSilently } = useAuth0()
-  const isFirstTime = true
+  const [isFirstTime, setIsFirstTime] = useState(false)
   if (isLoading) {
     return (
       <div className='App'>
-      <header className='App-header'>
-        <Nav />
-        <div className='content profile'>
-          <div className='container profile'>
-            <div className='profile-information'>
-              <div className='top-row'>
-                <div className='left'>
-                  <h2>Profile Information</h2>
-                  <p>Personalize your profile here.</p>
+        <header className='App-header'>
+          <Nav />
+          <div className='content profile'>
+            <div className='container profile'>
+              <div className='profile-information'>
+                <div className='top-row'>
+                  <div className='left'>
+                    <h2>Profile Information</h2>
+                    <p>Personalize your profile here.</p>
+                  </div>
+                  <div className='right'>
+                    <FontAwesomeIcon icon={faEdit} />
+                  </div>
                 </div>
-                <div className='right'>
-                  <FontAwesomeIcon icon={faEdit} />
-                </div>
-              </div>
-              <img src={'/assets/placeholder.jpg'} alt={'placeholder imag'} />
+                <img src={'/assets/placeholder.jpg'} alt={'placeholder imag'} />
                 <div className='profile-info'>
                   <h3 className='loading'></h3>
                   <p className='loading p'></p>
                 </div>
+              </div>
             </div>
+            <Listings response={[]} />
           </div>
-          <Listings response={[]}/>
-        </div>
-      </header>
-    </div>
+        </header>
+      </div>
     )
   }
 
@@ -48,8 +48,17 @@ const Profile = (): React.ReactElement => {
       console.log(e)
       return
     }
-
-    console.log(token)
+    const response = await fetch(`http://localhost:8080/user/${user?.sub}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    if (response.status === 404) {
+      setIsFirstTime(true)
+    } else {
+      setIsFirstTime(false)
+    }
   }, [user])
 
   useEffect(() => {
@@ -86,16 +95,16 @@ const Profile = (): React.ReactElement => {
                   : null
               }
               <img src={user?.picture} alt={user?.name} />
-                <div className='profile-info'>
-                  <h3>{user?.name}</h3>
-                  <p>{user?.email}</p>
-                </div>
-                <div className='more-about-profile'>
-                  <p><strong>1st Year:</strong> Aerospace Engineering</p>
-                </div>
+              <div className='profile-info'>
+                <h3>{user?.name}</h3>
+                <p>{user?.email}</p>
+              </div>
+              <div className='more-about-profile'>
+                <p><strong>1st Year:</strong> Aerospace Engineering</p>
+              </div>
             </div>
           </div>
-          <Listings response={[]}/>
+          <Listings response={[]} />
         </div>
       </header>
     </div>
@@ -121,13 +130,13 @@ export default withAuthenticationRequired(Profile, {
                 </div>
               </div>
               <img src={'/assets/placeholder.jpg'} alt={'placeholder imag'} />
-                <div className='profile-info'>
-                  <h3 className='loading'></h3>
-                  <p className='loading p'></p>
-                </div>
+              <div className='profile-info'>
+                <h3 className='loading'></h3>
+                <p className='loading p'></p>
+              </div>
             </div>
           </div>
-          <Listings response={[]}/>
+          <Listings response={[]} />
         </div>
       </header>
     </div>
