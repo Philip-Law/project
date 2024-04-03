f99f2a8c10754e6d595d52b2b6086d0d47bd0463
Mathew-Estafanous <mathewestafanous13@gmail.com>
2024-04-03 19:43:53 -0400

    enable ssl in prod environments

--- server/src/configs/db.ts
+++ server/src/configs/db.ts
@@ -8,6 +8,7 @@ const AppDataSource = new DataSource({
   type: 'postgres',
   port: 5432,
   url: process.env.DATABASE_URL,
+  ssl: process.env.ENVIRONMENT === 'prod',
   synchronize: false,
   logging: ['warn', 'error'],
   entities: [User, Post, Conversation, Message],
